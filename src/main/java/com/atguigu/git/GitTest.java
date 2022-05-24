e84f23d9d34231e83d1064ee11015b094a84a781
sdd <sdd@atguigu.com>
2022-05-24 20:22:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-05-24 20:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helo git!");
+    }
+}
