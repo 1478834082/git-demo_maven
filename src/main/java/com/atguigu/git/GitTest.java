efe80fa992c0f760cd5a0e7590fbf15b84f1f6ee
sdd <sdd@atguigu.com>
2022-05-24 20:47:12 +0800

    master_sdd commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("helo git!");
+        System.out.println("helo git!dfsafdsa");
+
     }
 }
